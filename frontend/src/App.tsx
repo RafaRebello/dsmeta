53163a3bc7ad54007795a0c795f2572ac8a5de43
RafaRebello <rafael.a.rebellodev@gmail.com>
2022-11-14 21:21:46 -0300

    Fisrt component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
+import NotificationButton from "./assets/components/NotificationButton";
+
 function App() {
-  return <h1>Olá munto!!</h1>;
+  return (
+    <>
+      <h1>Olá munto!!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  );
 }
 
 export default App;
